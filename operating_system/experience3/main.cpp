36e5ab2b7e050d9c329b335a36055b6ed4724669
finn33 <38375734@qq.com>
2020-11-18 09:12:57 +0800

    11.18

---
+++ operating_system/experience3/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<signal.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int wait_flag;
+
+void stop(){wait_flag=0;}
+ 
+int main()
+{
+	int pid1,pid2;
+	signal(2,stop);
+	pause();
+	while((pid1 = fork())== -1);
+	if(pid1 > 0)
+	{
+	  while((pid2 = fork()) == -1 );
+	  if(pid2 > 0)
+	  {
+	    wait_flag=1;
+	    sleep(5);  
+	    kill(pid1, 16);
+	    kill(pid2,17);
+	    wait(0);
+	    wait(0);
+	    printf("\nparent process is killed!!\n");
+	    exit(0);
+	  }
+      else
+   	  {
+	    wait_flag= 1;
+		signal(17,stop);
+	    printf("\nchild process 2 is killed by parent!!\n");
+	    exit(0);
+	  }
+	}
+	else 
+	{
+	  wait_flag=1;
+	  signal(16,stop);
+	  printf("\nchild process 1 is killed by parent!!\n");
+	}
+	
+}
+ 
