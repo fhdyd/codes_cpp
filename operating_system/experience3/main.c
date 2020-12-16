dd3e8f9118d537c141d7b67e3b11ea3a5a612ab2
finn33 <38375734@qq.com>
2020-12-16 08:54:40 +0800

    12.16

--- operating_system/experience3/main.c
+++ operating_system/experience3/main.c
@@ -1,51 +1,55 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include<sys/wait.h>
-#include<signal.h>
-#include<stdlib.h>
- 
-int wait_flag;
-void stop();
- 
-int main()
-{
-	int pid1,pid2;
-	signal(2,stop);
-	pause();
-	while((pid1 = fork())== -1);
-	if(pid1 > 0)
-	{
-	  while((pid2 = fork()) == -1 );
-	  if(pid2 > 0)
-	  {
-	    wait_flag= 1;
-	    sleep(5);  //----------------------//
-	    kill(pid1, 16);
-	    kill(pid2, 17);
-	    wait(0);
-	    wait(0);
-	    printf("\nParent process is killed!!\n");
-	    exit(0);
-	  }
-      else
-   	  {
-	    wait_flag= 1;
-		signal(17,stop);
-	    printf("\nChild process 2 is killed by parent!!\n");
-	    exit(0);
-	  }
-	}
-	else 
-	{
-	  wait_flag= 1;
-	  signal(16,stop);
-	  printf("\nChild process 1 is killed by parent!!\n");
-	}
-	
+#include <unistd.h>
+#include <sys/types.h>
+
+int flag_wait=1;
+
+void stop2()
+{   
+    flag_wait=0;
+    printf("\nson interruption\n");
 }
- 
 void stop()
 {
-	wait_flag = 0;
+    printf("\ninterruption\n");
+}
+
+int main()
+{
+    pid_t pid1,pid2;
+    signal(3,stop);
+
+    while((pid1=fork())==-1);
+    if(pid1>0)
+    {
+        while((pid2=fork())==-1);
+        if(pid2>0)
+        {
+            sleep(5);
+            kill(pid1,16);
+            wait(0);
+            kill(pid2,17);
+            wait(0);
+            printf("\nParent process is killed\n");
+            exit(0);
+        }
+        else
+        {
+            signal(17,stop2);
+            while(flag_wait);
+            printf("\nchild process 2 is killed\n");
+            exit(0);
+        }
+    }
+    else
+    {
+        signal(16,stop2);
+        while(flag_wait);
+        printf("\nchild process 1 is killed\n");
+        exit(0);
+    }
+    return 0;
 }
