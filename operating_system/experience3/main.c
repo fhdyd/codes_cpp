0358f96e13cf099ba163d16396869a4935b9c1fb
finn33 <38375734@qq.com>
2020-12-16 08:42:59 +0800

    12.16

--- operating_system/experience3/main.c
+++ operating_system/experience3/main.c
@@ -4,14 +4,13 @@
 #include<sys/wait.h>
 #include<signal.h>
 #include<stdlib.h>
-
+ 
 int wait_flag;
-
-void stop(){wait_flag=0;}
+void stop();
  
 int main()
 {
-	pid_t pid1,pid2;
+	int pid1,pid2;
 	signal(2,stop);
 	pause();
 	while((pid1 = fork())== -1);
@@ -20,29 +19,33 @@ int main()
 	  while((pid2 = fork()) == -1 );
 	  if(pid2 > 0)
 	  {
-	    wait_flag=1;
-	    sleep(5);  
+	    wait_flag= 1;
+	    sleep(5);  //----------------------//
 	    kill(pid1, 16);
-	    kill(pid2,17);
+	    kill(pid2, 17);
 	    wait(0);
 	    wait(0);
-	    printf("\nparent process is killed!!\n");
+	    printf("\nParent process is killed!!\n");
 	    exit(0);
 	  }
       else
    	  {
 	    wait_flag= 1;
 		signal(17,stop);
-	    printf("\nchild process 2 is killed by parent!!\n");
+	    printf("\nChild process 2 is killed by parent!!\n");
 	    exit(0);
 	  }
 	}
 	else 
 	{
-	  wait_flag=1;
+	  wait_flag= 1;
 	  signal(16,stop);
-	  printf("\nchild process 1 is killed by parent!!\n");
+	  printf("\nChild process 1 is killed by parent!!\n");
 	}
 	
 }
  
+void stop()
+{
+	wait_flag = 0;
+}
